40261a5fa4244b0b360ed328db384e0f9f1fae7b
dineshkumar7031 <141705801+dineshkumar7031@users.noreply.github.com>
2023-10-02 11:59:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "dinesh7031"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "dinesh7031-dynamo-db-table"
   }
 }
